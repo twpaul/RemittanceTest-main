3b23f72eb558ca470cb42bbf41437db4a85a3a1c
tcpaul <tcpaul@bajile.net>
2026-04-16 23:19:41 +0800

    feat: 依賴注入配置

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSingleton<RemittanceTest.Services.IRemittanceService, RemittanceTest.Services.RemittanceService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
